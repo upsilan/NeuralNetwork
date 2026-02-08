0dc883a7e5f42a8b3e81eb210fb5eeecdf42d1dd
Upsilan <danielbental162@gmail.com>
2026-02-08 12:33:57 +0200

    master

--- Program.cs
+++ Program.cs
@@ -2,11 +2,59 @@
 
 namespace Neural_Network
 {
-    class NeuralNetwork
+    class Program
     {
         static void Main(string[] args)
         {
+            NeuralNetwork nn = new NeuralNetwork(3, 5, 2);
+        }
+    }
 
+
+    class NeuralNetwork
+    {
+        Layer[] layers;
+        public NeuralNetwork(params int[] layerSizes)
+        {
+            layers = new Layer[layerSizes.Length];
+            for(int i = 1; i < layers.Length; i++)
+            {
+                layers[i] = new Layer(layerSizes[i - 1] ,layerSizes[i]);
+            }
+        }
+
+        class Layer
+        {
+            double[,] weights;
+            double[] biases;
+            double[] nodeArray;
+
+            int n_inputs;
+            int n_nodes;
+
+            public Layer(int n_inputs, int n_nodes)
+            {
+                weights = new double[n_nodes, n_inputs];
+                biases = new double[n_nodes];
+                nodeArray = new double[n_nodes];
+
+                this.n_nodes = n_nodes;
+                this.n_inputs = n_inputs;
+            }
+
+            public void Forward(double[] inputs)
+            {
+                //go for all nodes
+                for(int i = 0; i < n_nodes; i++)
+                {
+                    //in each node go through all inputs
+                    for(int j = 0; j < inputs.Length; j++)
+                    {
+                        nodeArray[i] += weights[i, j] * inputs[j];       
+                    }
+                    nodeArray[i] += biases[i];
+                }
+            }
         }
     }
 
--- obj/Debug/net10.0/Neural Network from scratch.AssemblyInfo.cs
+++ obj/Debug/net10.0/Neural Network from scratch.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Neural Network from scratch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e42e132aced9574b9eceb9b232cfd396bb17920")]
 [assembly: System.Reflection.AssemblyProductAttribute("Neural Network from scratch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Neural Network from scratch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
